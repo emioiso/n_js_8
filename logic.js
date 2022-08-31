b5c3006f63cd4bc9c85e73c708a4e16372598e99
emioiso <jijjjiijiijijiji@gmail.com>
2022-08-31 10:32:56 +0900

    n_js_8

---
+++ logic.js
@@ -0,0 +1,54 @@
+let age = 16;
+let isMember = true;
+let result = null;
+if (age <= 15) {
+  result = 800;
+} else if (isMember) {
+  result = 1000;
+} else {
+  result = 1800;
+}
+document.write(result);s
+
+/*let number = 3;
+if (number % 3 === 0) {
+  console.log(`サァン！`);
+} else {
+  console.log(number);
+}*/
+
+
+/*let age = 65;
+let isMember = true
+let result = null;
+if(age >= 60 && isMember){
+  result = 'シニア会員割引の対象です';
+}else{
+result ='シニア会員割引の対象ではありません';
+}
+document.write(result);*/
+
+
+
+/*
+let age = 65;
+let isMember = true;
+let result = null;
+if (age >= 60 && isMember) {
+  result = 'シニア会員割引の対象です';
+} else {
+  result = 'シニア会員割引の対象ではありません';
+}
+document.write(result);*/
+
+
+
+
+/*let age = 16
+let result =null
+if (age >= 18){
+  result = '成人';
+}else{
+  result ='未成年'
+}
+document.write(result);*/
